ff56b07cd32e6ccf981ab46a64186ae9337d3c4a
annh <longthan366@gmail.com>
2023-11-13 16:20:58 +0700

    add ui

--- app/src/main/java/com/longtv/zappy/ui/login/LoginFragment.java
+++ app/src/main/java/com/longtv/zappy/ui/login/LoginFragment.java
@@ -1,9 +1,17 @@
 package com.longtv.zappy.ui.login;
 
+import android.widget.EditText;
+
 import com.longtv.zappy.R;
 import com.longtv.zappy.base.BaseFragment;
 
+import butterknife.BindView;
+
+
 public class LoginFragment extends BaseFragment<LoginPresenter, LoginActivity> implements LoginView{
+    @BindView(R.id.edt_email)
+    protected EditText edtPhone;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_login;
